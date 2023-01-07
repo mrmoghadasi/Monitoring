b705814f9abe1fd58da03fb19f4f1c897d9b027b
MohamadReza Moghadasi <30479983+mrmelec@users.noreply.github.com>
2023-01-07 10:50:18 +0330

    Update DeployBlackBox_Probe.sh

--- DeployBlackBox_Probe.sh
+++ DeployBlackBox_Probe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /etc/blackbox_exporter/
-wget -O /etc/blackbox_exporter/ https://raw.githubusercontent.com/mrmelec/Monitoring/main/blackbox_exporter/blackbox.yml
+wget -p /etc/blackbox_exporter/ https://raw.githubusercontent.com/mrmelec/Monitoring/main/blackbox_exporter/blackbox.yml
 docker run --rm -d -p 9115:9115 --name blackbox_exporter -v `pwd`:/config prom/blackbox-exporter:master --config.file=/etc/blackbox_exporter//blackbox.yml
