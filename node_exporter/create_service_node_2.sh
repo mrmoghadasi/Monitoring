bec7154ff4c2e30d972fbe2d15d01d95793d390a
MohamadReza Moghadasi <30479983+mrmelec@users.noreply.github.com>
2023-01-14 12:28:03 +0330

    Create create_service_node_2.sh

---
+++ node_exporter/create_service_node_2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo '[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter --collector.systemd  --collector.textfile.directory /var/lib/node_exporter/textfile_collector/
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/node_exporter.service
+
+# enable node_exporter in systemctl
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl enable node_exporter
